257e290288fb3994358f757b20410367141bf34f
ajreno95 <A.RENO@vikes.csuohio.edu>
2018-01-21 19:05:03 -0500

    Update main.py

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,14 +1,18 @@
 import pigpio
 import time
 
+
 pi1 = pigpio.pi()
+#I decided to use a dictionary to declare color values to make the code more readable 
 COLORS = {'BLUE': 13, 'RED': 19, 'GREEN': 26}
 
+#Cycle method that turns brightness on and off for given color
+#colors is a pointer incase multiple arguements are passed to create a different color
 def cycle(*colors):
     for level in range(1, 256):
         for color in colors:
             pi1.set_PWM_dutycycle(color, level)
-    time.sleep(0.00196)
+        time.sleep(0.00196)
     for level in range(255, 0, -1):
         for color in colors:
             pi1.set_PWM_dutycycle(color, level)
